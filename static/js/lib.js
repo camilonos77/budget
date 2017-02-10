fcf6d7d03b336ba4602252186f693cc5a753e8e2
camilonosdocs@gmail.com <camilonosdocs@gmail.com>
2017-02-10 12:35:47 -0500

    actualizacion

--- static/js/lib.js
+++ static/js/lib.js
@@ -1,9 +1,35 @@
   $(document).ready(function($) {
+
+  		load();
+
+
         $("#btn_export_doc").click(function(event) {
+
+
 
         	$("#name_client_value").html($("#name_client").val());
 
+        	$("#name_service_value").html($("#name_service").val());
+
+        	$("#description_service_value").html($("#descripcion").val());
+
+			
+			$("#requirements_service_value").html($("#requirements").val());
+
+
+			$("#warranty_service_value").html($("#time_warranty").val());
+
+		
             $("#content_budget").wordExport("budget");
             return false;
         });
     });
+
+
+  function load(){
+
+  	for(var cont = 1;cont<30;cont++){
+
+  		$("#time_warranty").append('<option value="'+cont+'"  >'+cont +' days'+'</option>')
+  	}
+  }
